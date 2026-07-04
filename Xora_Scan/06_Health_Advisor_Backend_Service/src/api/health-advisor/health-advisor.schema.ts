f6e67c040a1dd6c933cad2b36fdc562645eba0d1
Dulaksha-Siriwardana <sujaniapsara75@gmail.com>
2026-07-04 11:27:30 +0530

    remove code

--- Xora_Scan/06_Health_Advisor_Backend_Service/src/api/health-advisor/health-advisor.schema.ts
+++
@@ -1,7 +0,0 @@
-import Joi from "joi";
-
-export const predictSchema = Joi.object({
-  identified_disease: Joi.string().required(),
-  disease_severity_from_xray: Joi.string().required(),
-  affected_teeth_count: Joi.number().integer().min(0).max(32).required(),
-});
